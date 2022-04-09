cbb03091c99614ce074380427d36ca96aefc0ffb
CMJones001 <c.m.jones001@gmail.com>
2022-04-09 19:49:20 +0200

    [feat] Multivectors with optional components

--- multivector.jl
+++ multivector.jl
@@ -1,37 +1,31 @@
-module Multivector
+module MultivectorComps
 
 import Base: +, -, *, /, ==, show, zero, iszero
-export Vec, Biv, Tri, ∧
+export Sca, Vec, Biv, Tri, ∧, ZeroVec
 
 # Classes for the geometric algebra of three dimensions
+abstract type Geometric end
+struct ZeroVec end
+const zv = ZeroVec()
 
 # Vector
-struct Vec{T<:Real} <: Number
+struct Vec{T<:Real} <: Geometric
     e1::T
     e2::T
     e3::T
 end
 
 # Bivector
-struct Biv{T<:Real} <: Number
+struct Biv{T<:Real} <: Geometric
     e12::T
     e23::T
     e31::T
 end
 
 # Pseudoscalar (trivector) element
-struct Tri{T<:Real}
+struct Tri{T<:Real} <: Geometric
     i::T
 end
-
-# Combination of all graded elements
-struct Mul{T<:Real} 
-    s::T
-    v::Vec{T}
-    b::Biv{T}
-    p::T
-end
-
 
 # Use v as the notation for the basis vectors, as e is used for exponentiation
 const v1 = Vec(1, 0, 0)
@@ -48,13 +42,17 @@ const T = Tri
 
 V(e1::Real, e2::Real, e3::Real) = V(promote(e1,e2,e3)...)
 B(e12::Real, e23::Real, e31::Real) = B(promote(e12,e23,e31)...)
-# T(i::Real) = T(promote(i)...)
+
 iszero(v::Vec) = iszero(v.e1) & iszero(v.e2) & iszero(v.e3)
 iszero(b::Biv) = iszero(b.e12) & iszero(b.e23) & iszero(b.e31)
 iszero(t::Tri) = iszero(t.i)
+iszero(::ZeroVec) = true
 
+zero(::Vec) = Vec(0, 0, 0)
+zero(::Biv) = Biv(0, 0, 0)
+zero(::Tri) = Tri(0)
 
-# Simple operations
+# addition and subtraction
 +(z::Vec, w::Vec) = V(z.e1 + w.e1, z.e2 + w.e2, z.e3 + w.e3)
 -(z::Vec, w::Vec) = V(z.e1 - w.e1, z.e2 - w.e2, z.e3 - w.e3)
 -(z::Vec) = V(-z.e1, -z.e2, -z.e3)
@@ -65,9 +63,22 @@ iszero(t::Tri) = iszero(t.i)
 -(z::Biv) = B(-z.e12, -z.e23, -z.e31)
 ==(z::Biv, w::Biv) = (z.e12 == w.e12) & (z.e23 == w.e23) & (z.e31 == w.e31) 
 
-+(z::Tri, w::Tri) = T(z.i + w.i)
--(z::Tri, w::Tri) = T(z.i - w.i)
--(z::Tri) = T(-z.i)
++(z::Tri, w::Tri) = Tri(z.i + w.i)
+-(z::Tri, w::Tri) = Tri(z.i - w.i)
+-(z::Tri) = Tri(-z.i)
+==(z::T, w::T) = z.i == w.i
+
+# Zero is the identity under addition
++(z::Geometric, ::ZeroVec) = z
++(::ZeroVec, z::Geometric) = z
+-(z::Geometric, ::ZeroVec) = z
+-(::ZeroVec, z::Geometric) =-z
++(::ZeroVec, ::ZeroVec) = zv
+-(::ZeroVec, ::ZeroVec) = zv
+
+# Zero vector is equal to zero...
+==(z::Geometric, ::ZeroVec) = iszero(z)
+==(::ZeroVec, z::Geometric) = iszero(z)
 
 # Scalar operations
 *(a::Real, z::Vec) = V(z.e1 * a, z.e2 * a, z.e3 * a)
@@ -82,20 +93,26 @@ iszero(t::Tri) = iszero(t.i)
 *(z::Tri, a::Real) = T(z.i * a)
 /(z::Tri, a::Real) = T(z.i / a)
 
+# Under multiplication, zero vector always returns zero
+*(a::Geometric, ::ZeroVec) = zv
+*(z::ZeroVec, a::Geometric) = zv
+*(::ZeroVec, ::ZeroVec) = zv
+*(::ZeroVec, r::Real) = zv
+*(r::Real, z::ZeroVec) = zv
+
 # Misc options
 show(io::IO, v::Vec) = print(io, "$(v.e1)×e1 + $(v.e2)×e2 + $(v.e3)×e3")
 show(io::IO, v::Biv) = print(io, "$(v.e12)×e1∧e2 + $(v.e23)×e2∧e3 + $(v.e31)×e3∧e1")
 show(io::IO, v::Tri) = print(io, "$(v.i)×e1∧e2∧e3")
+show(io::IO, ::ZeroVec) = print(io, "∅")
 
 # Outer product with scalars is just multiplication
-∧(z::Vec, a::Real) = z*a
-∧(a::Real, z::Vec) = z*a
+∧(z::Geometric, a::Real) = z*a
+∧(a::Real, z::Geometric) = z*a
 
-∧(z::Biv, a::Real) = z*a
-∧(a::Real, z::Biv) = z*a
-
-∧(z::Tri, a::Real) = z*a
-∧(a::Real, z::Tri) = z*a
+# Outer product with Nothing gives nothing
+∧(z::Geometric, ::ZeroVec) = zv
+∧(::ZeroVec, ::Geometric) = zv
 
 # Outer product of vectors gives a bivector
 ∧(z::Vec, w::Vec) = Biv(
@@ -106,5 +123,61 @@ show(io::IO, v::Tri) = print(io, "$(v.i)×e1∧e2∧e3")
 
 ∧(b::Biv, v::Vec) = Tri(b.e12 * v.e3 + b.e23 * v.e1 + b.e31 * v.e2)
 ∧(v::Vec, b::Biv) = Tri(b.e12 * v.e3 + b.e23 * v.e1 + b.e31 * v.e2)
+∧(::ZeroVec, ::ZeroVec) = zv
+
+end
+
+module Multivector
+using ..MultivectorComps
+
+import Base: +, -, *, /, ==, show, zero, iszero
+import ..MultivectorComps: V, B, T, v1, v2, v3, v12, v23, v31, I, ∧, zv, Geometric
+export V, B, T, v1, v2, v3, v12, v23, v31, I
+export Vec, Biv, Tri, ∧, Mul, zv
+
+struct Mul{T<:Real}
+    s::Union{T, ZeroVec}
+    v::Union{Vec{T}, ZeroVec}
+    b::Union{Biv{T}, ZeroVec}
+    p::Union{Tri{T}, ZeroVec}
+end
+
+∧(z::Mul, w::Geometric) = z ∧ Mul(w)
+∧(z::Geometric, w::Mul) = Mul(z) ∧ w
+
+Mul(s::Real) = Mul(s, zv, zv, zv)
+Mul(v::Vec)  = Mul(zv, v, zv, zv)
+Mul(b::Biv)  = Mul(zv, zv, b, zv)
+Mul(t::Tri)  = Mul(zv, zv, zv, t)
+Mul() = Mul{Real}(zv, zv, zv, zv)
+# We need to define a generic type when all values are zero still
+Mul(::ZeroVec, ::ZeroVec, ::ZeroVec, ::ZeroVec) = Mul{UInt8}(zv, zv, zv, zv)
+
+==(m::Mul, s::Real) = (m.s == s) & iszero(m.b) & iszero(m.v) & iszero(m.p)
+==(b::Real, m::Mul) = (m.s == s) & iszero(m.b) & iszero(m.v) & iszero(m.p)
+==(m::Mul, v::Vec) = (m.v == v) & iszero(m.s) & iszero(m.b) & iszero(m.p)
+==(v::Vec, m::Mul) = (m.v == v) & iszero(m.s) & iszero(m.b) & iszero(m.p)
+==(m::Mul, b::Biv) = (m.b == b) & iszero(m.s) & iszero(m.v) & iszero(m.p)
+==(b::Biv, m::Mul) = (m.b == b) & iszero(m.s) & iszero(m.v) & iszero(m.p)
+==(m::Mul, p::Tri) = (m.p == p) & iszero(m.b) & iszero(m.v) & iszero(m.s)
+==(p::Tri, m::Mul) = (m.p == p) & iszero(m.b) & iszero(m.v) & iszero(m.s)
+
+# Convert to multivectors if no other addition rules are available
++(z::Geometric, w::Geometric) = Mul(z) + Mul(w)
+-(z::Geometric, w::Geometric) = Mul(z) - Mul(w)
+
+# This is likely to cause problems elsewhere?!
+# Maybe replace with a NoVec type
++(z::Mul, w::Mul) = Mul(z.s + w.s, z.v + w.v, z.b + w.b, z.p + w.p)
+-(z::Mul, w::Mul) = Mul(z.s - w.s, z.v - w.v, z.b - w.b, z.p - w.p)
+==(z::Mul, w::Mul) = (z.s == w.s) & (z.v == w.v) & (z.b == w.b) & (z.p == w.p)
+iszero(m::Mul) = iszero(m.s) & iszero(m.v) & iszero(m.b) & iszero(m.p)
+
+∧(z::Mul, w::Mul) = Mul(
+    z.s * w.s,
+    z.s * w.v + z.v * w.s,
+    z.s * w.b + z.b * z.s + z.v ∧ w.v,
+    z.s * w.p + z.p * w.s + z.v ∧ w.b + z.b ∧ w.v
+)
 
 end
--- runtest-multivector.jl
+++ runtest-multivector.jl
@@ -36,6 +36,9 @@ import .Multivector: V, B, T, v1, v2, v3, v12, v23, v31, I
         # Basis vectors
         @test B(4, 2, 3) == 4v12 + B(0, 2, 3)
         @test B(4, 2, 3) == 4v12 + 2v23 + 3v31
+
+        @test 3v1 ∧ 3v2 - 3v12 isa Biv
+        @test 3v1 ∧ 3v2 - 3v12 - 3v23 == 3(2v12 - v23)
     end
 end
 
@@ -61,6 +64,45 @@ end
 
         @test B(1, 3, 4) ∧ V(1, 0, -1) == T(2)
         @test (1v12 + 3v23 + 4v31) ∧ (v1 - v3) == 2I
+    end
+end
+
+@testset "Multivector General" begin
+    @test Mul(2) ∧ Mul(3) == Mul(6)
+
+    @testset "Vector combinations" begin
+        @test Mul(2) ∧ Mul(v1) == Mul(2v1)
+        @test Mul(v1) ∧ Mul(v2) == Mul(v12)
+        @test Mul(4v2) ∧ Mul(v1) == Mul(-4v12)
+        @test Mul(v1) + Mul(2v2)  == Mul(V(1, 2, 0))
+    end
+
+    @testset "Parallel vectors" begin
+        @test iszero(Mul(v2) ∧ Mul(v2)) 
+        @test Mul(v2) ∧ Mul(v2) == Mul()
+        @test Mul(v1) ∧ Mul(4v1) == Mul()
+    end
+
+    @testset "Multivectors" begin
+        @test Mul(2) ∧ Mul(4v31) == Mul(8v31)
+        @test Mul(v31) ∧ Mul(v2) == Mul(Tri(1))
+        @test Mul(v1) ∧ Mul(v23) == Mul(I)
+        @test Mul(4v1) ∧ Mul(2v23) == Mul(8I)
+
+        @test Mul(v31) ∧ Mul(v1) == Mul()
+        @test Mul(v31) ∧ Mul(32) == Mul()
+    end
+
+    @testset "Cross components" begin
+        @test Mul(2) ∧ Mul(v1) == 2v1
+        @test Mul(4v1) ∧ Mul(2v23) == 8I
+        @test Mul(4v1) ∧ 2v23 == 8I
+        @test Mul(4v1) ∧ 2v23 == 8I
+    end
+
+    @testset "Cross addition" begin
+        @test v1 + I == Mul(zv, v1, zv, I)
+        @test 3v1 + 4v2 - 2I == Mul(zv, V(3, 4, 0), zv, -2I)
     end
 end
 end
